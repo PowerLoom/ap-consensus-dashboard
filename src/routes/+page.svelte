fb35c5042a4464b80bbfd60c162bc632a2f3366f
Akshay Dahiya <akshay@powerloom.io>
2024-02-10 19:02:31 +0530

    chore: initial changes done to use Ui helper API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import { env } from '$env/dynamic/public';
     import { ethers } from "ethers";
 	import project_ids from '../projects.json';
+	import { get } from 'svelte/store';
 	export let API_PREFIX = env.PUBLIC_API_PREFIX;
     //export let NETWORK = env.PUBLIC_NETWORK || "Ethereum Mainnet";
     export let EXPLORER_PREFIX = env.PUBLIC_EXPLORER_PREFIX || "https://etherscan.io";
@@ -1286,8 +1287,56 @@
 	let currentEpochId = "";
     let allSnapshotters= [];
 	let allMasterSnapshotters= [];
+	let search = "";
+	let finalizedProjects;
+	let epochs;
+	let prevSearch = "";
+	let data = {};
+	let API = "http://localhost:9030"
+
+	$: {
+		if (search != prevSearch){
+			localStorage.setItem("search_term", search);
+			prevSearch = search;
+		}
+	}
+	async function getFinalizedProjects(epochId){
+		let response;
+		try {
+			response = await axios.post(API+`/getFinalizedProjects`, {epochId: epochId});
+			console.log('got Finalized Projects', response.data);
+			if (response.data) {
+				return new Set(response.data.finalized_projects);
+			} else {
+				throw new Error(JSON.stringify(response.data));
+			}
+		}
+		catch (e){
+			console.error('Unable to get finalized projects', e);
+		}
+	}
+
+	async function getSubmittedProjects(epochId, snapshotterAddr){
+		let response;
+		try {
+			response = await axios.post(API+`/getSubmittedProjects`, {epochId: epochId, snapshotterAddr: snapshotterAddr});
+			console.log('got Submitted Projects', response.data);
+			if (response.data) {
+				return response.data.submitted_projects;
+			} else {
+				throw new Error(JSON.stringify(response.data));
+			}
+		}
+		catch (e){
+			console.error('Unable to get submitted projects', e);
+		}
+	}
+
+
 
     onMount(async () => {
+      search = localStorage.getItem("search_term");
+	  console.log('search', search);
       currentEpoch = Object.assign({}, await contract.currentEpoch());
       currentEpochId = Number(currentEpoch[2]);
 	  currentEpoch = Number(currentEpoch[1]);
@@ -1298,7 +1347,7 @@
 	  allSnapshotters = allSnapshotters.concat(allMasterSnapshotters);
 	  let getProjects = project_ids["project_ids"];
 
-	  const projectSnapshotters = Object.values(Object.assign({}, await contract.getSnapshotters()));
+	  const projectSnapshotters = Object.values(Object.assign({}, allSnapshotters));
 	  console.log('current getProjects', getProjects);
       for (let i=0; i<getProjects.length; i++){
         let proj = {
@@ -1307,24 +1356,17 @@
           }
           projects = [...projects, proj];
       }
-      /*
-      const resp = await axios.get(API_PREFIX+'/metrics/projects');
-      console.log('resp', resp.data);
-      //console.warn('block', await provider.getBlockNumber());
-      for (let i=0; i<resp.data.length; i++){
-          const snapshotters = await axios.get(API_PREFIX+'/metrics/'+resp.data[i]+'/snapshotters');
-          console.log(snapshotters.data);
-          //allSnapshotters = [...new Set([...allSnapshotters, ...snapshotters.data.snapshotters])];
-          let proj = {
-              id: resp.data[i],
-              snapshotters: snapshotters.data.snapshotters
-          }
-          projects = [...projects, proj];
-          if (i==99){
-            break;
-          }
-      }
-      //*/
+
+	  epochs = [currentEpochId, currentEpochId-1, currentEpochId-2, currentEpochId-3, currentEpochId-4];
+	  console.log('epochs', epochs);
+
+	  for (let i=0; i<epochs.length; i++){
+		let finalizedProjects = await getFinalizedProjects(epochs[i]);
+		let submittedProjects = await getSubmittedProjects(epochs[i], search);
+		data[epochs[i]] = {finalizedProjects: finalizedProjects, submittedProjects: submittedProjects};
+	  }
+	  console.log('data', data);
+
     });
 </script>
 <div>
@@ -1347,56 +1389,68 @@
     </dl>
 </div>
 <div class="overflow-hidden bg-white shadow sm:rounded-md">
-    <ul role="list" class="divide-y divide-gray-200">
-      {#each projects as project}
-      <li>
-        <a href="/projects/{project.id}" class="block hover:bg-gray-50">
-          <div class="flex items-center px-4 py-4 sm:px-6">
-            <div class="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-              <div class="truncate">
-                <div class="flex text-sm">
-                  <p class="truncate font-medium text-indigo-600">{project.id.split(":")[0]} for {project.id.split(":")[1]}</p>
-                  <p class="ml-1 flex-shrink-0 font-normal text-gray-500">in {project.id.split(":")[2]}</p>
-                </div>
-                {#if 0}
-                <div class="mt-2 flex">
-                  <div class="flex items-center text-sm text-gray-500">
-                    <!-- Heroicon name: mini/calendar -->
-                    <svg class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                      <path fill-rule="evenodd" d="M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z" clip-rule="evenodd" />
-                    </svg>
-                    <p>
-                      Synced up to
-                      <time datetime="2020-01-07">January 7, 2020</time>
-                    </p>
-                  </div>
-                </div>
-                {/if}
-              </div>
-              <div class="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
-                <div class="flex -space-x-1 overflow-hidden">
-                  {#each project.snapshotters as snapshotter}
-                  <img class="inline-block h-6 w-6 rounded-full ring-2 ring-white" alt="{snapshotter}" title="{snapshotter}" src="https://api.dicebear.com/7.x/identicon/png?seed={snapshotter}">
-                  {/each}
-                <!--
-                  <img class="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Lindsay Walton">
-  
-                  <img class="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Courtney Henry">
-  
-                  <img class="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Tom Cook">
-                -->
-                </div>
-              </div>
-            </div>
-            <div class="ml-5 flex-shrink-0">
-              <!-- Heroicon name: mini/chevron-right -->
-              <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-              </svg>
-            </div>
-          </div>
-        </a>
-      </li>
-      {/each}
-    </ul>
-  </div>
+	<div class="relative mt-2 flex items-center">
+		<input type="text" name="search" id="search" bind:value={search} placeholder="Search snapshotters" class="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+		<div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5" on:click={() => {search="";}}>
+		  <kbd class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">⌫</kbd>
+		</div>
+	  </div>
+</div>
+{#if search !=  ""}
+	{#each Object.entries(data) as [key, value]}
+	<div class="overflow-hidden bg-white shadow sm:rounded-md">
+		<div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+			<dt class="truncate text-sm font-medium text-gray-500">Epoch</dt>
+			<dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{key}</dd>
+		</div>
+		<ul role="list" class="divide-y divide-gray-200 mb-4">
+			{#each value.submittedProjects as project}
+			<li>
+				<a href="/projects/{project}" class="block hover:bg-gray-50">
+				  <div class="flex items-center px-4 py-4 sm:px-6">
+					<div class="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+					  <div class="truncate">
+						<div class="flex text-sm">
+						  <p class="truncate font-medium text-indigo-600">{project.split(":")[0]} for {project.split(":")[1]}</p>
+						  <p class="ml-1 flex-shrink-0 font-normal text-gray-500">in {project.split(":")[2]}</p>
+						</div>
+						
+					  </div>
+					  <div class="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
+						<div class="flex -space-x-1 overflow-hidden">
+						  {#if value.finalizedProjects.has(project)}
+						  <span class="inline-block h-6 w-6 rounded-full bg-green-100">
+							<svg class="h-6 w-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+							</svg>
+						  </span>
+						  {/if}
+						  {#if !value.finalizedProjects.has(project)}
+						  <span class="inline-block h-6 w-6 rounded-full bg-red-100">
+							<svg class="h-6 w-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+							</svg>
+						  </span>
+						  {/if}
+						</div>
+					  </div>
+					</div>
+					<div class="ml-5 flex-shrink-0">
+					  <!-- Heroicon name: mini/chevron-right -->
+					  <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+						<path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+					  </svg>
+					</div>
+				  </div>
+				</a>
+			  </li>
+			{/each}
+		</ul>
+	
+	</div>
+	{/each}
+{/if}
+
+{#if search ==  ""}
+<h3 class="text-lg font-medium leading-6 text-gray-900">Please Enter your snapshotter Address</h3>
+{/if}
